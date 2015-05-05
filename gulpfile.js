2d6baae6aab850b05f8d91e1c6109e5605bd223f
renelopez <rene.lopez.cano@gmail.com>
2015-05-05 00:49:43 -0500

    Added Sass and LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp=require('gulp');
 var karma=require('karma').server;
 var jshint=require('gulp-jshint');
 var stylish=require('jshint-stylish');
+var browserSync=require('browser-sync');
+var sass=require('gulp-sass');
 
 
 gulp.task('test',function(done){
@@ -19,3 +21,32 @@ gulp.task('jshint',function(){
        .pipe(jshint())
        .pipe(jshint.reporter(stylish));
 });
+
+gulp.task('serve',['watch'],function(){
+   browserSync({
+       server:{
+           baseDir:'./src'
+       }
+
+   })
+});
+
+gulp.task('sass',function(){
+   return gulp.src('src/styles/app.scss')
+       .pipe(sass())
+       .pipe(browserSync.reload({stream:true}))
+       .pipe(gulp.dest('src/styles'));
+});
+
+gulp.task('reload',function(){
+    browserSync.reload();
+});
+
+gulp.task('watch',function(){
+   gulp.watch('src/styles/**/*.scss',['sass']);
+    gulp.watch('src/**/*.html',['reload']);
+    gulp.watch('src/*/*.js',['reload']);
+
+});
+
+gulp.task('default',['serve']);
